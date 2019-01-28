41ea5d5d42e593ca4ec29f65be8937a369f6b45c
HeyCamCam <cameronchubb@outlook.com>
2019-01-28 05:39:30 -0800

    calculator can now round to the hundreths place

--- tip-calculator.py
+++ tip-calculator.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python3
 
-meal_total = int(input("How much was your meal?: "))
+meal_total = float(input("How much was your meal?: "))
 print("Meal is $" + str(meal_total))
 tip_percent = int(input("What percentage would you like to tip?: "))
 print("Tip percentage is " + str(tip_percent) + "%")
-tip_total = meal_total * tip_percent / 100
+tip_total = round(meal_total * tip_percent / 100, 2)
 
 print("You should tip $" + str(tip_total))
-
